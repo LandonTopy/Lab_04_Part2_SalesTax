8cc04e2b8421cd2cd6394bf77adeb1482195e851
LandonTopy <landontopy@gmail.com>
2023-09-14 09:43:57 -0400

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double productPrice;
+        double salesTax = 1.05;
+        double totalPrice;
+
+        System.out.println("What was the price?");
+        productPrice = scan.nextDouble();
+        totalPrice = productPrice * salesTax;
+        System.out.println("The total price is $" + totalPrice);
+
     }
 }
